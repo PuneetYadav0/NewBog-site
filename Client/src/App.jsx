d17ff9cb147ce9c0abc0a782a634f01438ffb143
PuneetYadav0 <Exampreparation9580@gmail.com>
2024-01-17 01:48:33 +0530

    Pages Created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import SignUp from "./pages/signUp";
+import SignIn from "./pages/signIn";
+import DashBoard from "./pages/dashBoard";
+import Projects from './pages/Projects'
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/dashBoard" element={<DashBoard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
